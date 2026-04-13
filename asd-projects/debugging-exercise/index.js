f725ac41643a22a715accfc2be658d8c259a65f7
TRcodes <tariq.ghuneim@inspire35.org>
2026-04-13 14:37:05 +0000

    Finished Debugging project - all ghosts are now behaving properly!

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -107,7 +107,7 @@ function update() {
   // to make seeing issues in the debugger slightly easier (in practice, you should use
   // ghosts.length, but do NOT change it here)
   for (var i = 0; i < maxGhosts; i++) {
-    var ghost = ghosts[j];
+    var ghost = ghosts[i];
 
     // move the ghost
     moveGhost(ghost);
@@ -129,7 +129,7 @@ function update() {
 
 // this moves ghosts in memory but doesn't update them on the screen
 function moveGhost(ghost) {
-  ghost.x = ghost.speedX;
+  ghost.x += ghost.speedX;
   ghost.y += ghost.speedY;
 }
 
@@ -153,13 +153,13 @@ function bounceGhost(ghost) {
   // this bounces off the bottom wall
   else if (ghost.y > boardHeight) {
     ghost.y -= ghost.speedY;
-    ghost.speedX *= -1;
+    ghost.speedY *= -1;
   }
 }
 
 // this redraws the ghost's position on the screen
 function updateGhostOnScreen(ghost) {
-  maxGhosts = 1;
+  maxGhosts = 10;
 
   // these lines redraw the ghost's position
   $(ghost.id).css("left", ghost.x);
